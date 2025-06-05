510526e0d9aa7785e1a62a98f5c1015c55a264ff
Griffin Faecher <griffin.faecher@tufts.edu>
2025-06-05 16:41:10 -0400

    Sign Extend Start

---
+++ Lab1/signExtend.sv
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module signExtend (
+        input logic [31:0] x,
+        input logic [9:0] opcode,
+        output logic [63:0] y
+);
+
+        always_comb begin
+
+                
+
+        end
+
+endmodule
